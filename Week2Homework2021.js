75693de82350bd79b93c53020cc91c912c93bf74
Clay Grisham <grishamclay@gmail.com>
2021-09-06 19:27:48 -0500

    added file
    updated file readme

---
+++ Week2Homework2021.js
@@ -0,0 +1,11 @@
+const pizzaPlace = 'Olives Pizza';
+const numberOfToppings = 10;
+
+if (numberOfToppings <= 10) {
+    console.log("Quality,not quantity");
+} else {
+    console.log("A whole lot of pizza");
+}
+
+
+
