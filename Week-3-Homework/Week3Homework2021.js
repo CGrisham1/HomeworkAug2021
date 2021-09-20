e62c0f272933cb6929a1df2839121f257b8ffb46
CGrisham1 <89211813+CGrisham1@users.noreply.github.com>
2021-09-20 16:46:35 -0500

    Updated for loop

---
+++ Week-3-Homework/Week3Homework2021.js
@@ -0,0 +1,31 @@
+const pizzaToppings = ["Meat" , "Cheese" , "Bacon" , "Tomato"];
+
+function greetCustomer() {
+    let greeting = `Welcome to Olive's Pizza, our toppings are: `;
+    for (let topping of pizzaToppings) {greeting += `${topping}, `;}  
+    console.log(greeting);
+}
+greetCustomer();
+
+
+
+
+
+
+
+
+
+
+// function getPizzaOrder(size, crust, ...toppings) {
+//     console.log("One large thick crust pizza with x , y , z ...")
+// };
+
+
+// const size = large
+// const crust = thin
+// const toppings = [Cheese, Meat, Bacon, Tomato];
+
+
+// function preparePizza([size , crust , ...toppings]) {
+    
+// };
